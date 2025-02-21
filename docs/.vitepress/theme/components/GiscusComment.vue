27700797b9b5c3bbe2f37fe6870113441afbf2e8
Mark LauQ <112486391@qq.com>
2025-02-21 19:59:25 +0800

    perf: support Giscus

---
+++ docs/.vitepress/theme/components/GiscusComment.vue
@@ -0,0 +1,27 @@
+<template>
+  <div style="margin-top: 24px">
+    <Giscus
+      id="comments"
+      repo="mark-lauq/mark-lauq.github.io"
+      repoid="R_kgDON8pnkQ"
+      category="Announcements"
+      categoryid="DIC_kwDON8pnkc4CnK-V"
+      mapping="title"
+      term="Welcome to giscus!"
+      reactionsenabled="1"
+      emitmetadata="0"
+      inputposition="top"
+      loading="lazy"
+      :theme="isDark ? 'dark' : 'light'"
+      :key="route.path"
+    ></Giscus>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Giscus from '@giscus/vue';
+import { useRoute, useData } from "vitepress";
+
+const route = useRoute();
+const { isDark } = useData();
+</script>
